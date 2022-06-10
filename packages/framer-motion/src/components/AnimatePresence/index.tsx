e2ec0de63eb0d25ec813f3138d65c92c2c5dd6c0
JaeSeoKim <devjaeseo@gmail.com>
2022-06-11 00:48:21 +0900

    Remove some comments

--- packages/framer-motion/src/components/AnimatePresence/index.tsx
+++ packages/framer-motion/src/components/AnimatePresence/index.tsx
@@ -121,7 +121,6 @@ export const AnimatePresence: React.FunctionComponent<React.PropsWithChildren<An
     const filteredChildren = onlyElements(children)
     let childrenToRender = filteredChildren
 
-    // Keep a living record of the children we're actually rendering so we
     const exitingChildren = useRef(
         new Map<ComponentKey, ReactElement<any>>()
     ).current
